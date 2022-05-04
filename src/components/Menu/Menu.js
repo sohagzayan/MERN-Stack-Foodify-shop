9cc63908cf5a247d95022444eac552fd3bfe6285
sohagmia360 <gdsohag360@gmail.com>
2022-05-04 16:05:46 +0600

    condesonal menuber finish

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,34 +1,71 @@
-import React from 'react';
-import { AiOutlineClose } from 'react-icons/ai';
-
-const Menu = ({showMenu , setShowMenu}) => {
-    return (
-        <div className=''>
-            <ul className={showMenu ? "flex items-center menuBer active" : "flex items-center menuBer "}>
-                <span className='CloseIcons cursor-pointer'>
-                    <AiOutlineClose onClick={()=> setShowMenu(!showMenu) } />
-                </span>
-                <li className='px-4 py-1'>
-                    <a className='   text-black_soft font-medium text-lg ' href="/">Home</a>
-                </li>
-                <li className='px-4 py-1'>
-                    <a className='   text-black_soft font-medium text-lg ' href="/">Blog</a>
-                </li>
-                <li className='px-4 py-1'>
-                    <a className='   text-black_soft font-medium text-lg ' href="/">Manage Product</a>
-                </li>
-                <li className='px-4 py-1'>
-                    <a className='   text-black_soft font-medium text-lg ' href="/">Add Product</a>
-                </li>
-                <li className='px-4 py-1'>
-                    <a className=' capitalize  text-black_soft font-medium text-lg ' href="/">My <span>Product</span></a>
-                </li>
-                <li className='px-4 py-1'>
-                    <a className='   text-black_soft font-medium text-lg' href="/">Logout</a>
-                </li>
-            </ul>
-        </div>
-    );
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+const Menu = ({ showMenu, setShowMenu }) => {
+  const user = false;
+  return (
+    <div className="">
+      <ul
+        className={
+          showMenu
+            ? "flex items-center menuBer active"
+            : "flex items-center menuBer "
+        }
+      >
+        <span className="CloseIcons cursor-pointer">
+          <AiOutlineClose onClick={() => setShowMenu(!showMenu)} />
+        </span>
+        <li className="px-4 py-1">
+          <NavLink className="   text-black_soft font-medium text-lg " to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="px-4 py-1">
+          <NavLink className="   text-black_soft font-medium text-lg " to="/">
+            Blog
+          </NavLink>
+        </li>
+        {user ? (
+          <li className="px-4 py-1">
+            <NavLink className="   text-black_soft font-medium text-lg " to="/">
+              Manage Product
+            </NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {user ? (
+          <li className="px-4 py-1">
+            <NavLink className="   text-black_soft font-medium text-lg " to="/">
+              Add Product
+            </NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        {user ? (
+          <li className="px-4 py-1">
+            <NavLink
+              className=" capitalize  text-black_soft font-medium text-lg "
+              to="/"
+            >
+              My <span>Product</span>
+            </NavLink>
+          </li>
+        ) : (
+          ""
+        )}
+        <li className="px-4 py-1">
+          <NavLink
+            className="   text-black_soft font-medium text-lg"
+            to="/register/login"
+          >
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Menu;
