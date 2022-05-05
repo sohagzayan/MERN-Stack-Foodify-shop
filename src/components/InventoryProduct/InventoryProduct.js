4a0d411fcd8549610cda84b1eb612522cf0f1e64
sohagmia360 <gdsohag360@gmail.com>
2022-05-05 14:05:45 +0600

    load data from saver and make api condesonaly

--- src/components/InventoryProduct/InventoryProduct.js
+++ src/components/InventoryProduct/InventoryProduct.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AiOutlineDoubleRight } from 'react-icons/ai';
 import { FiHeart } from 'react-icons/fi';
-import image from '../../assets/image/blog-1.jpg';
+// import image from '../../assets/image/blog-1.jpg';
 
-const InventoryProduct = () => {
+const InventoryProduct = ({item}) => {
+    const {image , foodCategory , quantity , price , supplier , title} = item
     return (
         <div className='mx-4 inventory_product'>
             <div className='pl-5 pr-5 mt-5'>
@@ -11,12 +12,12 @@ const InventoryProduct = () => {
             </div>
             <div className='p-5'>
                 <span className='card-date'>March.02.2021</span>
-                <h4 className='tracking-wider font-light'>Breakfast Cereals, Snacks</h4>
-                <h3 className='text-2xl mb-2 font-bold'>Aenean Magna, A Lobortis Mauris Vehicula Sed.</h3>
-                <h5 className='mb-2'><span className='text-lg font-semibold '>Supplier :</span> Jonker Mahabub </h5>
+                <h4 className='tracking-wider font-light'>{foodCategory}</h4>
+                <h3 className='text-2xl mb-2 font-bold'>{title}</h3>
+                <h5 className='mb-2'><span className='text-lg font-semibold '>Supplier :</span>{supplier}</h5>
                 <div className='flex items-center justify-between mb-3'>
-                    <span className='text-3xl text-green_soft font-semibold'>$9</span>
-                    <span className='text-md text-green_soft tracking-wider'><span className='text-3xl font-bold'>Q</span>uantity : <span className='text-2xl font-bold'>69</span></span>
+                    <span className='text-3xl text-green_soft font-semibold'>${price}</span>
+                    <span className='text-md text-green_soft tracking-wider'><span className='text-3xl font-bold'>Q</span>uantity : <span className='text-2xl font-bold'>{quantity}</span></span>
                 </div>
                 <div className='flex items-center justify-between mb-4'>
                     <button className='btn-product flex items-center'>Update <AiOutlineDoubleRight className='text-lg ml-5 ' /> </button>
@@ -28,3 +29,94 @@ const InventoryProduct = () => {
 };
 
 export default InventoryProduct;
+
+// [
+//     {
+//       "image" : "https://i.postimg.cc/pL6rxZWb/pro-img-03.jpg" ,
+//       "foodCategory" : "Breakfast Cereals",
+//       "title" : " Sounded like a good title for a board for multi-varieties of apples..", 
+//       "supplier" : "Leanne Graham" ,
+//       "price" : "4",
+//       "quantity" : "30"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/26p5LSL0/pro-img-07.jpg" ,
+//       "foodCategory" : "dryed almod",
+//       "title" : "Fresh dryed almod (30gm)", 
+//       "supplier" : "Ervin Howell" ,
+//       "price" : "7",
+//       "quantity" : "10"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/t4TJLfHN/pro-img-1.jpg" ,
+//       "foodCategory" : "litchi",
+//       "title" : "Fresh litchi 100% organic.", 
+//       "supplier" : "Jonker Mahabub" ,
+//       "price" : "3",
+//       "quantity" : "25"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/SxJQZMWZ/pro-img-11.jpg" ,
+//       "foodCategory" : "guava",
+//       "title" : "Sp.red fresh guava", 
+//       "supplier" : "Samantha" ,
+//       "price" : "15",
+//       "quantity" : "30"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/nzvh4fvp/pro-img-2.jpg" ,
+//       "foodCategory" : "Eggplant",
+//       "title" : "Fresh & healty Eggplant food", 
+//       "supplier" : "Karianne" ,
+//       "price" : "12",
+//       "quantity" : "33"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/wMgTF4VJ/pro-img-4.jpg" ,
+//       "foodCategory" : "litchi",
+//       "title" : "Fresh litchi 100% organic and fresh food", 
+//       "supplier" : "Chelsey Dietrich" ,
+//       "price" : "20",
+//       "quantity" : "12"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/sxmfLP6L/pro-img-5.jpg" ,
+//       "foodCategory" : "fresh tomato",
+//       "title" : "Vegetable tomato fresh and teste", 
+//       "supplier" : "Leopoldo_Corkery" ,
+//       "price" : "26",
+//       "quantity" : "44"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/gcH0rdzK/pro-img-6.jpg" ,
+//       "foodCategory" : "grassbean",
+//       "title" : "Natural grassbean and total new item", 
+//       "supplier" : "Elwyn.Skiles" ,
+//       "price" : "50",
+//       "quantity" : "17"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/CKn1Kq5Z/pro-img-7-1.jpg" ,
+//       "foodCategory" : "dryed almod",
+//       "title" : "new product Fresh dryed almod (50gm)", 
+//       "supplier" : "Elwyn.Skiles" ,
+//       "price" : "50",
+//       "quantity" : "17"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/656pvSTx/pro-img-8.jpg" ,
+//       "foodCategory" : "juice",
+//       "title" : "Orange juice (5ltr) foraner item", 
+//       "supplier" : "Elwyn.Skiles" ,
+//       "price" : "8",
+//       "quantity" : "22"
+//     },
+//     {
+//       "image" : "https://i.postimg.cc/FsPsgZJd/pro-img-9.jpg" ,
+//       "foodCategory" : "coconet",
+//       "title" : "Organic coconet (5ltr) juice fresh", 
+//       "supplier" : "Elwyn.Skiles" ,
+//       "price" : "37",
+//       "quantity" : "22"
+//     },
+// ]
--- src/components/Invertory/Inventory.js
+++ src/components/Invertory/Inventory.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import InventoryProduct from '../InventoryProduct/InventoryProduct';
 import './Inventory.css';
 const Inventory = () => {
+
+    const [allData , setAllData] = useState([])
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/api/todo?limit=6`)
+        .then(res => res.json())
+        .then(data => setAllData(data))
+    },[])
+
     return (
         <div className='mt-16 container mx-auto'>
             <h2 className='text-center text-4xl  md:text-5xl font-bold text-black_soft tracking-wider '>Our Inventory </h2>
            <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2    mt-16'>
-                <InventoryProduct   />
-                <InventoryProduct   />
-                <InventoryProduct   />
-                <InventoryProduct   />
-                <InventoryProduct   />
-                <InventoryProduct   />
+            {allData.map(item => <InventoryProduct 
+            key={item._id}
+                item={item}
+            />)}
+                
            </div>
             <div className='text-center'>
             <button className='btn-inventory mt-6'>
